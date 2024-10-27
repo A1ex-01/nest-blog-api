814f29c041333b2566d54de7212d4dec0e94f28c
a1ex <peterxhhx@gmail.com>
2024-10-28 00:07:13 +0800

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/ResponseInterceptor';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -9,6 +9,15 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 允许的HTTP方法
     allowedHeaders: '*', // 允许所有请求头
   });
+  const config = new DocumentBuilder()
+    .setTitle('接口文档')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    jsonDocumentUrl: 'openApiJson',
+  });
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(8004);
 }
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { PostService } from './post.service';
-
+@ApiTags('文章')
 @Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Get()
+  @ApiOperation({ summary: '文章列表' })
+  @ApiQuery({ name: 'current', required: false })
+  @ApiQuery({ name: 'pageSize', required: false })
   findAll(@Query('current') current = 1, @Query('pageSize') pageSize = 10) {
     return this.postService.findAll({
       current: Number(current),
@@ -14,6 +18,7 @@ export class PostController {
   }
 
   @Get(':uuid')
+  @ApiOperation({ summary: '文章详情' })
   findOne(@Param('uuid') uuid: string) {
     return this.postService.findOne(uuid);
   }
