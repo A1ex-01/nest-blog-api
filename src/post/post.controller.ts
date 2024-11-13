863863b4bbb6eb60a542154449a4b3079c185ab4
a1ex <peterxhhx@gmail.com>
2024-11-13 21:49:56 +0800

    fix: bugfix

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -6,8 +6,8 @@ import { PostService } from './post.service';
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @Get()
   @ApiOperation({ summary: '文章列表' })
+  @Get()
   @ApiQuery({ name: 'current', required: false })
   @ApiQuery({ name: 'pageSize', required: false })
   findAll(@Query('current') current = 1, @Query('pageSize') pageSize = 10) {
@@ -17,8 +17,8 @@ export class PostController {
     });
   }
 
-  @Get(':uuid')
   @ApiOperation({ summary: '文章详情' })
+  @Get(':uuid')
   findOne(@Param('uuid') uuid: string) {
     return this.postService.findOne(uuid);
   }
